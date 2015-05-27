72109e1b65442249683a6c238b14abcaf366b5ae
milesmarchant <miles@marchant.net>
2015-05-27 15:48:48 -0700

    Created Drivey, the subsystem representing the drivetrain.

---
+++ org/bitbuckets/frc2015OffSeason/subsystems/Drivey.java
@@ -0,0 +1,34 @@
+package org.bitbuckets.frc2015OffSeason.subsystems;
+
+public class Drivey extends StateSubsystem{
+
+	public Drivey(String name, long iterationTime) {
+		super(name, iterationTime);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void setupComponents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupTriggers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
