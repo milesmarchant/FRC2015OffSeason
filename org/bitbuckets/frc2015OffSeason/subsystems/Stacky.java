0e5bdc8ba55613e938865fef25b39bfd4453bfbc
milesmarchant <miles@marchant.net>
2015-05-27 15:51:31 -0700

    Added Stacky, a subsystem representing the stacker mechanism.

---
+++ org/bitbuckets/frc2015OffSeason/subsystems/Stacky.java
@@ -0,0 +1,34 @@
+package org.bitbuckets.frc2015OffSeason.subsystems;
+
+public class Stacky extends StateSubsystem{
+
+	public Stacky(String name, long iterationTime) {
+		super(name, iterationTime);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void setupComponents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupTriggers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
